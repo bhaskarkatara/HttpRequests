ba063f4d011fdfec1d765d98d66fc350424b2b7c
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-06-26 16:36:12 +0530

    final Changes

--- app/src/main/java/com/example/httprequests/MainActivity.kt
+++ app/src/main/java/com/example/httprequests/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.httprequests.ui.theme.HttpRequestsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HttpRequestsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   RecipeScreen(
-//                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+              Surface (modifier = Modifier.padding(top =50.dp)){
+                  RecipeScreen()
+              }
+
                 }
             }
         }
     }
-}
+
 
