b8e3122575bc55e7ce80d32b3ace63455d600ad0
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-06-26 16:42:00 +0530

    final Changes

--- app/src/main/java/com/example/httprequests/MainActivity.kt
+++ app/src/main/java/com/example/httprequests/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,7 +22,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HttpRequestsTheme {
-              Surface (modifier = Modifier.padding(top =50.dp)){
+              Surface (modifier = Modifier.fillMaxSize(),
+                  color = MaterialTheme.colorScheme.background
+                  ){
                   RecipeScreen()
               }
 
