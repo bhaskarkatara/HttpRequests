236280e3feaa4c57a2aa192c63f87e6d975997d3
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-06-26 17:07:56 +0530

    final Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,9 @@ dependencies {
 
 
 
+//    implementation "androidx.compose.ui:ui/:1.0.0"
+//    implementation "androidx.compose.foundation:foundation:1.0.0"
+//    implementation "androidx.compose.material3:material3:1.0.0"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/httprequests/RecipeScreen.kt
+++ app/src/main/java/com/example/httprequests/RecipeScreen.kt
@@ -1,18 +1,24 @@
 package com.example.httprequests
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -62,6 +68,7 @@ fun CategoryScreen(categories: List<Category>) {
 @Composable
 
 fun CategoryItem(category: Category) {
+    val showDialog = remember { mutableStateOf(false) }
   Column(modifier = Modifier
       .padding(8.dp)
       .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
@@ -70,6 +77,10 @@ fun CategoryItem(category: Category) {
           modifier = Modifier
               .fillMaxSize()
               .aspectRatio(1f)
+              .clickable {
+                  showDialog.value = true
+              }
+
       )
 
 
@@ -79,5 +90,24 @@ fun CategoryItem(category: Category) {
          modifier = Modifier.padding(top = 4.dp)
          )
 
+      if (showDialog.value) {
+          AlertDialog(
+              onDismissRequest = { showDialog.value = false },
+              confirmButton = {
+                  Button(onClick = { showDialog.value = false }) {
+                      Text("OK")
+                  }
+              },
+              text = {
+                  LazyColumn(modifier = Modifier.padding(10.dp)) {
+                      item {
+                          Text(text = category.strCategoryDescription)
+                      }
+                  }
+              }
+          )
+      }
+      }
+
+
   }
-}
